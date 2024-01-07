c785ed14e821d42270b68b5f1861c29fb23bd7bc
vxncetxn <thevancetan@gmail.com>
2024-01-08 00:42:18 +0800

    Added Tailwind Capsize and serif font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["PP Acma", ...defaultTheme.fontFamily.serif],
+      },
+    },
+    fontMetrics: {
+      serif: {
+        capHeight: 780,
+        ascent: 1032,
+        descent: -480,
+        lineGap: 0,
+        unitsPerEm: 1000,
+      },
+    },
+  },
+  plugins: [require("tailwindcss-capsize")],
+};
